3200b2ac36d1ef845a267cd553ec30b9d2b78cd6
Tanizawa Kenta <kt10025102@gmail.com>
2023-11-07 17:56:29 +0000

    Add input field

--- one-shot-vocab/src/app/vocabulary/page.tsx
+++ one-shot-vocab/src/app/vocabulary/page.tsx
@@ -1,12 +1,26 @@
-import styles from "./page.module.css";
+"use client";
 
-import {Input, IconButton, SearchIcon } from "@/plugins/chakra-ui-modules";
+import styles from "./page.module.css";
+import { useState } from "react";
+import { Input, IconButton, SearchIcon } from "@/plugins/chakra-ui-modules";
 
 export default function Vocabulary() {
+  const [englishWord, setEnglishWord] = useState("");
+  const handleChange = (event: any) => setEnglishWord(event.target.value);
+  const searchEnglishWord = () => {
+    console.log("searchEnglishWord");
+  };
+
   return (
-    <div className={styles.searchField}>
-      <Input placeholder="Basic usage" />
-      <IconButton aria-label="Search database" icon={<SearchIcon />} />
+    <div className={styles.vocabularyContainer}>
+      <div className={styles.searchField}>
+        <Input
+          value={englishWord}
+          onChange={handleChange}
+          placeholder="英単語を入力してください。"
+        />
+        <IconButton aria-label='SearchIcon' icon={<SearchIcon />} onClick={searchEnglishWord} />
+      </div>
     </div>
   );
 }

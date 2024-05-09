b9e854084accffa4d695cc9c539d1931c6119863
ktanizaw <kt10025102@gmail.com>
2024-05-09 23:14:51 +0100

    add show more button feature

--- one-shot-vocab/src/app/vocabulary/parts/wordDetailsCard.tsx
+++ one-shot-vocab/src/app/vocabulary/parts/wordDetailsCard.tsx
@@ -4,13 +4,17 @@ import {
   Text,
   Stack,
   HStack,
+  VStack,
+  Box,
   Card,
   CardHeader,
   CardBody,
   Heading,
   Wrap,
   WrapItem,
+  Button,
 } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface WordDetails {
   pronunciation?: {
@@ -43,112 +47,166 @@ const WordDetailsCard: React.FC<WordDetailsCardProps> = ({
     return null;
   }
 
+  const [showAllResults, setShowAllResults] = useState(false);
+  const toggleShowAllResults = () => {
+    setShowAllResults(!showAllResults);
+  };
+
   return (
     <Center p={4}>
-      <Card w={800} bg="gray.50">
-        <CardHeader>
-          <Heading size="md">{englishWord}</Heading>
-          <Text>{wordDetails?.pronunciation?.all}</Text>
-          <Text as="i">{wordDetails?.results[0]?.partOfSpeech}</Text>
-        </CardHeader>
-
-        <CardBody pt={0}>
-          {wordDetails?.results.map((result: any, resultIndex: number) => (
-            <Stack key={resultIndex}>
-              <Text as="b">{resultIndex + 1}.</Text>
-              <HStack>
-                <Text fontSize="sm" minWidth={12}>
-                  意味:
-                </Text>
-                <Text>{result?.definition}</Text>
-              </HStack>
-
-              {result?.examples && (
-                <HStack>
-                  <Text fontSize="sm" minWidth={12}>
-                    例文:
+      <VStack spacing={4}>
+        <Card w={{ base: '100%', md: 800 }} bg="gray.50">
+          <CardHeader>
+            <Heading size={{ base: 'sm', md: 'md' }}>{englishWord}</Heading>
+            <Text fontSize={{ base: 'sm', md: 'md' }}>
+              {wordDetails?.pronunciation?.all}
+            </Text>
+          </CardHeader>
+          <CardBody pt={0}>
+            {wordDetails?.results
+              .slice(0, showAllResults ? undefined : 3)
+              .map((result: any, resultIndex: number) => (
+                <Stack key={resultIndex}>
+                  <Text as="b" fontSize={{ base: 'sm', md: 'md' }}>
+                    {resultIndex + 1}.
                   </Text>
-                  <Wrap>
-                    {result.examples.map((example: string, index: number) => (
-                      <WrapItem key={index}>
-                        {example}
-                        {index < result?.examples.length - 1 && ' | '}
-                      </WrapItem>
-                    ))}
-                  </Wrap>
-                </HStack>
-              )}
+                  <HStack>
+                    <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                      品詞:
+                    </Text>
+                    <Text as="i" fontSize={{ base: 'sm', md: 'md' }}>
+                      {result?.partOfSpeech}
+                    </Text>
+                  </HStack>
 
-              {result?.synonyms && (
-                <HStack>
-                  <Text fontSize="sm" minWidth={12}>
-                    類義語:
-                  </Text>
-                  <Wrap>
-                    {result.synonyms.map((synonym: string, index: number) => (
-                      <WrapItem key={index}>
-                        {synonym}
-                        {index < result.synonyms.length - 1 && ', '}
-                      </WrapItem>
-                    ))}
-                  </Wrap>
-                </HStack>
-              )}
+                  <HStack>
+                    <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                      意味:
+                    </Text>
+                    <Text fontSize={{ base: 'sm', md: 'md' }}>
+                      {result?.definition}
+                    </Text>
+                  </HStack>
 
-              {result?.similarTo && (
-                <HStack>
-                  <Text fontSize="sm" minWidth={12}>
-                    類似語:{' '}
-                  </Text>
-                  <Wrap>
-                    {result.similarTo.map((similar: string, index: number) => (
-                      <WrapItem key={index}>
-                        {similar}
-                        {index < result.similarTo.length - 1 && ', '}
-                      </WrapItem>
-                    ))}
-                  </Wrap>
-                </HStack>
-              )}
+                  {result?.examples && (
+                    <HStack>
+                      <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                        例文:
+                      </Text>
+                      <Wrap>
+                        {result.examples.map(
+                          (example: string, index: number) => (
+                            <WrapItem
+                              key={index}
+                              fontSize={{ base: 'sm', md: 'md' }}
+                            >
+                              {example}
+                              {index < result?.examples.length - 1 && ' | '}
+                            </WrapItem>
+                          ),
+                        )}
+                      </Wrap>
+                    </HStack>
+                  )}
 
-              {result?.antonyms && (
-                <HStack>
-                  <Text fontSize="sm" minWidth={12}>
-                    対義語:
-                  </Text>
-                  <Wrap>
-                    {result.antonyms.map((antonym: string, index: number) => (
-                      <WrapItem key={index}>
-                        {antonym}
-                        {index < result.antonyms.length - 1 && ', '}
-                      </WrapItem>
-                    ))}
-                  </Wrap>
-                </HStack>
-              )}
+                  {result?.synonyms && (
+                    <HStack>
+                      <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                        類義語:
+                      </Text>
+                      <Wrap>
+                        {result.synonyms.map(
+                          (synonym: string, index: number) => (
+                            <WrapItem
+                              key={index}
+                              fontSize={{ base: 'sm', md: 'md' }}
+                            >
+                              {synonym}
+                              {index < result.synonyms.length - 1 && ', '}
+                            </WrapItem>
+                          ),
+                        )}
+                      </Wrap>
+                    </HStack>
+                  )}
 
-              {result?.derivation && (
-                <HStack>
-                  <Text fontSize="sm" minWidth={12}>
-                    派生語:
-                  </Text>
-                  <Wrap>
-                    {result.derivation.map(
-                      (derivation: string, index: number) => (
-                        <WrapItem key={index}>
-                          {derivation}
-                          {index < result.derivation.length - 1 && ', '}
-                        </WrapItem>
-                      ),
-                    )}
-                  </Wrap>
-                </HStack>
-              )}
-              <Divider mb={2} />
-            </Stack>
-          ))}
-        </CardBody>
-      </Card>
+                  {result?.similarTo && (
+                    <HStack>
+                      <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                        類似語:{' '}
+                      </Text>
+                      <Wrap>
+                        {result.similarTo.map(
+                          (similar: string, index: number) => (
+                            <WrapItem
+                              key={index}
+                              fontSize={{ base: 'sm', md: 'md' }}
+                            >
+                              {similar}
+                              {index < result.similarTo.length - 1 && ', '}
+                            </WrapItem>
+                          ),
+                        )}
+                      </Wrap>
+                    </HStack>
+                  )}
+
+                  {result?.antonyms && (
+                    <HStack>
+                      <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                        対義語:
+                      </Text>
+                      <Wrap>
+                        {result.antonyms.map(
+                          (antonym: string, index: number) => (
+                            <WrapItem
+                              key={index}
+                              fontSize={{ base: 'sm', md: 'md' }}
+                            >
+                              {antonym}
+                              {index < result.antonyms.length - 1 && ', '}
+                            </WrapItem>
+                          ),
+                        )}
+                      </Wrap>
+                    </HStack>
+                  )}
+
+                  {result?.derivation && (
+                    <HStack>
+                      <Text fontSize={{ base: 'xs', md: 'sm' }} minWidth={12}>
+                        派生語:
+                      </Text>
+                      <Wrap>
+                        {result.derivation.map(
+                          (derivation: string, index: number) => (
+                            <WrapItem
+                              key={index}
+                              fontSize={{ base: 'sm', md: 'md' }}
+                            >
+                              {derivation}
+                              {index < result.derivation.length - 1 && ', '}
+                            </WrapItem>
+                          ),
+                        )}
+                      </Wrap>
+                    </HStack>
+                  )}
+                  <Divider mb={2} />
+                </Stack>
+              ))}
+          </CardBody>
+        </Card>
+        {wordDetails?.results.length > 3 && (
+          <Button
+            colorScheme="blue"
+            variant={showAllResults ? 'outline' : 'solid'}
+            onClick={toggleShowAllResults}
+          >
+            {showAllResults ? '閉じる' : 'もっと見る'}
+          </Button>
+        )}
+      </VStack>
     </Center>
   );
 };
